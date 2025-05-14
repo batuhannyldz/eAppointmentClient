5c3e223bc37b87e336ca9ceebb023d26ca3e9942
batuhan <batuhan.yildiz@isetia.com>
2025-05-14 14:17:58 +0300

    Doctor create completed

--- src/app/components/layouts/navbar/navbar.component.ts
+++ src/app/components/layouts/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 constructor(
-  private router: Router
+  private router: Router,
+  public auth: AuthService
+
 ){}
 signOut(){
   localStorage.removeItem("token");
