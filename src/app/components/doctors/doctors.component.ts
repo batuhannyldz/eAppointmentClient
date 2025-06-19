159160ce90a44fe3634863e1a799c4d96dc2cde9
batuhan <batuhan.yildiz@isetia.com>
2025-06-19 11:35:59 +0300

    feat:doctor delete

--- src/app/components/doctors/doctors.component.ts
+++ src/app/components/doctors/doctors.component.ts
@@ -54,17 +54,6 @@ export class DoctorsComponent implements OnInit {
     }
   }
 
-  delete(id: string, fullName: string){
-    this.swal.callSwal("Delete doctor?",`You want to delete ${fullName}?`,()=> {
-      this.http.post<string>("Doctors/DeleteById", {id: id}, (res)=> {
-        this.swal.callToast(res.data,"info");
-        this.getAll();
-      })
-    })
-  }
-
-  
-
   update(form:NgForm){
     if(form.valid){
       this.http.post<string>("Doctors/Update",this.updateModel,(res)=> {
@@ -73,5 +62,15 @@ export class DoctorsComponent implements OnInit {
         this.updateModalCloseBtn?.nativeElement.click();        
       });
     }
+  }
+
+  delete(id: string,fullName: string){
+    this.swal.callSwal("Delete Doctor?",`You Want To Delete ${fullName}?`,()=>{
+      this.http.post<string>("Doctors/DeleteById",{id:id},(res)=>{
+        this.swal.callToast(res.data,"info");
+        this.getAll();
+      })
+    })
+
   }
 }
