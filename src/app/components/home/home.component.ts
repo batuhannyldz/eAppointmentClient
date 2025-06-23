f4d1c81bf32fab32d82091ee553770db23149778
batuhan <batuhan.yildiz@isetia.com>
2025-06-23 16:16:32 +0300

    feat:Create Appointment Home Page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { departments } from '../../constants';
+import { DoctorModel } from '../../models/doctor.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DxSchedulerModule } from 'devextreme-angular';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule,CommonModule,DxSchedulerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  departments = departments;
+  doctors: DoctorModel[] = [];
+
+  selectedDepartmentValue: number = 0;
+  selectedDoctorId: string = "";
 
 }
