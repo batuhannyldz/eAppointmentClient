a7d1a8b7a46dbcd10ab6cc99960dc28489993851
batuhan <batuhan.yildiz@isetia.com>
2025-07-15 13:54:25 +0300

    feat:user crud operatıons

--- src/app/components/layouts/navbar/navbar.component.ts
+++ src/app/components/layouts/navbar/navbar.component.ts
@@ -4,18 +4,19 @@ import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-constructor(
-  private router: Router,
-  public auth: AuthService
+  constructor(
+    private router: Router,
+    public auth: AuthService
+  ){}
 
-){}
-signOut(){
-  localStorage.removeItem("token");
-  this.router.navigateByUrl("/login");
-}
+  signOut(){
+    localStorage.removeItem("token");
+    this.router.navigateByUrl("/login");
+  }
 }
